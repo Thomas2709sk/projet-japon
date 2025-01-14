abf2163b69caccf6070f83f94ec1a126e204e4e1
Thomas <noaillesthomas@gmail.com>
2025-01-14 17:14:10 +0100

    add script for time

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+let time = document.getElementById('time');
+setInterval(() => {
+    let d = new Date();
+    let options = { timeZone: 'Asia/Tokyo', weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let timeString = d.toLocaleTimeString( 'fr-FR', { timeZone: 'Asia/Tokyo' });
+    let dateString = d.toLocaleDateString('fr-FR', options);
+    time.innerHTML =`Date et heure de Tokyo: ${dateString} - ${timeString}`;
+}, );
