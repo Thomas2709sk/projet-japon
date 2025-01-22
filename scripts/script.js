e5143dbb135cc7ddda14b961c7e54721143c4818
Thomas <noaillesthomas@gmail.com>
2025-01-22 18:34:20 +0100

    add Weather api script

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,51 @@ setInterval(() => {
     let dateString = d.toLocaleDateString('fr-FR', options);
     time.innerHTML =`Date et heure de Tokyo: ${dateString} - ${timeString}`;
 }, );
+
+
+// SCRIPT WEATHER
+
+// Affiche la méétéo actuelle via API OpenWeatherMap
+
+const apiKey = 'd408b2585e4efe44f7725a1691a98ff2';
+
+async function recupDonees(city, callback) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=fr`);
+        const data = await response.json();
+        return callback(data);
+    } catch {
+        console.log("error");
+    }
+}
+
+// Fonction pour rechercher data dans l'API
+function affTemp(dt) {
+    // affiche température
+    const temperature = Math.round (dt.main.temp);
+    // affiche météo
+    const description = dt.weather[0].description;
+    // cherche le code pour l'icone météo
+    const iconCode = dt.weather[0].icon;  
+
+    // URL de l'icône météo
+    const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+    // ID de la div dans HTML
+    let weather = document.getElementById('meteo');
+
+    // Créer l'élément image
+    const imgElement = document.createElement('img');
+    imgElement.src = iconUrl;
+    imgElement.alt = description;
+
+
+    weather.innerHTML = `
+    Météo à Tokyo: ${temperature} °C - ${description}
+    <img src="${iconUrl}" alt="${description}" width="50"> `;
+    
+}
+
+// Appel de la fonction avec 'Tokyo' comme ville
+recupDonees('Tokyo', affTemp);
+
