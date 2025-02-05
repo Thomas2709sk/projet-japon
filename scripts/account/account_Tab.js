f6233f39fec52dadd2e2133780cc9cc912fca9ff
Thomas <noaillesthomas@gmail.com>
2025-02-05 18:08:57 +0100

    add acount page wip

---
+++ scripts/account/account_Tab.js
@@ -0,0 +1,48 @@
+// Change les onglets dans mon compte
+
+// Récupére les <li>
+const menuLinks = document.querySelectorAll('.nav-account');
+
+// Supprime class "active"
+function removeActiveClassFromAll() {
+    menuLinks.forEach(link => {
+        link.classList.remove('active');
+    });
+}
+
+// 1fficher ou cacher les sections en fonction du click
+function showContent(contentId) {
+    const contentElements = document.querySelectorAll('.tab-content');
+    contentElements.forEach(content => {
+        if (content.id === contentId) {
+            content.style.display = 'block'; 
+        } else {
+            content.style.display = 'none';
+        }
+    });
+}
+
+// Créer évenement "click"
+menuLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();  
+
+        removeActiveClassFromAll();
+
+        // Classe active au lien cliqué
+        e.currentTarget.classList.add('active');
+
+        // Onglet sélectionné
+        const contentId = e.currentTarget.id.replace('Link', 'Content');
+        showContent(contentId);
+    });
+});
+
+//  Mon compte par défaut
+document.addEventListener('DOMContentLoaded', () => {
+    showContent('accountContent');
+    const firstLink = document.querySelector('#accountLink');
+    if (firstLink) {
+        firstLink.classList.add('active');
+    }
+});
